24d49c1bc5bd8718a67b2fa50cb2630586ee1c59
Bedel93300 <abdelhafid93300@gmail.com>
2017-09-29 19:10:56 +0200

    MAJ étape 6

--- src/main/java/co/simplon/factorielle/Factorielle.java
+++ src/main/java/co/simplon/factorielle/Factorielle.java
@@ -1,23 +1,30 @@
 package co.simplon.factorielle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * Factorielle
  */
 public class Factorielle 
 {
+	static final Logger logger = LoggerFactory.getLogger(Factorielle.class);
+
 	public Factorielle() {
 		super();
 	}
 
 	public long calculer(long n) {
+		logger.info("Passage dans la méthode calculer !");
+		logger.debug("Passage dans la méthode calculer !");
+		logger.warn("Passage dans la méthode calculer !");
 		if (n > 1) {
-			System.out.println("Valeur de n>1 : "+n); 
 			return n * calculer(n-1);
 		} else if (n < 0) {
 			throw new IllegalArgumentException();
 		}
-		System.out.println("Valeur de n<=1 : "+n);  
-		
+				
 		return 1;
 	}
 
--- src/main/java/co/simplon/factorielle/Log.java
+++ src/main/java/co/simplon/factorielle/Log.java
@@ -1,16 +1,37 @@
 package co.simplon.factorielle;
 
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Log {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 private static Logger log = LoggerFactory.getLogger(Log.class);
+	 
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, IllegalArgumentException
+	   {
+		System.out.println("\n\nBegin\n");
+	      log.info("hello info");
+	      log.warn("warning  1 !!!");
+	      
+	      
 		Factorielle f=new Factorielle();
+		log.info("Calcul du factoriel de 0");
 		long resultat=f.calculer(0);
-		System.out.println("Résultat de !0 est "+resultat);
+		log.info("Calcul du factoriel de 1");
 		resultat=f.calculer(1);
-		System.out.println("Résultat de !1 est "+resultat);
+		log.info("Calcul du factoriel de 2");
 		resultat=f.calculer(2);
-		System.out.println("Résultat de !2 est "+resultat);
-	}
+		log.info("Calcul du factoriel de -1");
+		resultat=f.calculer(2);
+		
+	   
+	      log.warn("Warning 2 !!!");
+	      log.info("goodbye info");
+	      System.out.println("\nEnd");
+	   }
 
 }
